aeb2cb107498c56381b279f4665ba4d8424d3359
sworc <lightbox@nifty.com>
2022-04-22 11:10:59 +0900

    Add files via upload

--- sample_01.py
+++ sample_01.py
@@ -28,6 +28,7 @@ type2 = str(type(data2))
 type3 = str(type(data3))
 type4 = str(type(data4))
 
+"""
 print( data1 + "<br>" )
 print( escape(type1) + "<br>" )
 
@@ -39,5 +40,30 @@ print( escape(type3) + "<br>" )
 
 print( str(data4) + "<br>" )
 print( escape(type4) + "<br>" )
+"""
 
+view = f"""<!DOCTYPE html>
+<html>
+<head>
+	<meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+	<meta charset="UTF-8">
+</head>
+<body>
+<div id="main">
+	<form method="get">
+		<div>
+			<div>氏名</div>
+			<div><input type="text" name="field1" value=""></div>
+		</div>
 
+		<div>
+			<div>フリガナ</div>
+			<div><input type="text" name="field2" value=""></div>
+			<div><input type="submit" name="send" value="送信"></div>
+		</div>
+	</form>
+</div>
+</body>
+</html>"""
+
+print( view )
